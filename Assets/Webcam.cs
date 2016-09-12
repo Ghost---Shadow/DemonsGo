ab2ae5236d8a94b34fa2754c9f67031df269e4eb
Ghost---Shadow <souradeep.nanda2014@vit.ac.in>
2016-09-12 09:41:09 +0530

    Initial commit

---
+++ Assets/Webcam.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Webcam : MonoBehaviour {
+
+	public float screenSize = 100.0f;
+	
+	private WebCamTexture webcamTexture;
+
+     void Start () 
+     {
+         webcamTexture = new WebCamTexture();
+		 webcamTexture.Play();
+		gameObject.GetComponent<Renderer>().material.mainTexture = webcamTexture;        
+     }
+	 
+	 void Update(){
+		 if(webcamTexture.width < 100)
+			return; // Camera not ready
+		
+		float aspect = (float)webcamTexture.width/(float)webcamTexture.height;
+		transform.localScale = new Vector3(aspect*screenSize,screenSize,1);
+	 }
+}
