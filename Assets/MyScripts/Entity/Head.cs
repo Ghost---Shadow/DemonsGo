13d36d46a2693c7091d9d46e54f9d252bc2f771f
Ghost---Shadow <souradeep.nanda2014@vit.ac.in>
2016-09-13 18:53:40 +0530

    Placeholders in place

---
+++ Assets/MyScripts/Entity/Head.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Head : MonoBehaviour {
+	public GameObject[] heads;	
+
+	private GameObject currentHead;
+
+	void Start(){
+		currentHead = null;
+	}
+
+	public void spawnRandomHead(){
+		
+	}
+}
