c5c771840d03d9f6620d80ef7a94131f82183d43
Ghost---Shadow <souradeep.nanda2014@vit.ac.in>
2016-09-15 07:55:48 +0530

    Autorotation and project name

---
+++ Assets/MyScripts/Global/Frustum.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Frustum : MonoBehaviour {
+	private Camera cam;
+    private Plane[] planes;
+    void Start() {
+        cam = Camera.main;
+        planes = GeometryUtility.CalculateFrustumPlanes(cam);
+        int i = 0;
+        while (i < planes.Length) {
+            GameObject p = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            p.name = "Plane " + i.ToString();
+            p.transform.position = -planes[i].normal * planes[i].distance;
+            p.transform.rotation = Quaternion.FromToRotation(Vector3.up, planes[i].normal);
+            i++;
+        }
+    }
+}
